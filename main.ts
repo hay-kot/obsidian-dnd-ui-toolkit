4dd640acd7701143539eb4c37199fd8b9b4bd044
Hayden <64056131+hay-kot@users.noreply.github.com>
2025-04-11 11:02:20 -0500

    update manifest and other default values

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { App, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Plugin, PluginSettingTab } from 'obsidian';
 import { StatsView } from 'lib/views/StatsView';
 import { AbilityScoreView } from 'lib/views/AbilityScoreView';
 import { BaseView } from 'lib/views/BaseView';
@@ -72,18 +72,6 @@ class DndSettingsTab extends PluginSettingTab {
 
 	display(): void {
 		const { containerEl } = this;
-
 		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
