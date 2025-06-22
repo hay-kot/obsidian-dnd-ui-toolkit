da5c005bfb309efcccd2d371f3928935d4baee1b
Hayden <64056131+hay-kot@users.noreply.github.com>
2025-06-22 13:54:33 -0500

    new file context abstraction for easier management on frontmatter

---
+++ lib/views/filecontext.ts
@@ -0,0 +1,27 @@
+import { Frontmatter } from "lib/types";
+import * as Fm from "lib/domains/frontmatter";
+import { App, MarkdownPostProcessorContext } from "obsidian";
+import { msgbus } from "lib/services/event-bus";
+
+/**
+ * useFileContext provides a way to access the frontmatter within a file. It also provides
+ * a way to subscribe to changes in the frontmatter.
+ * */
+export function useFileContext(app: App, ctx: MarkdownPostProcessorContext) {
+  function frontmatter(): Frontmatter {
+    const fm = app.metadataCache.getCache(ctx.sourcePath)?.frontmatter;
+    return Fm.anyIntoFrontMatter(fm || {});
+  }
+
+  function onFrontmatterChange(cb: (v: Frontmatter) => void): () => void {
+    return msgbus.subscribe(ctx.sourcePath, "fm:changed", cb);
+  }
+
+  return {
+		filepath: ctx.sourcePath,
+    frontmatter,
+    onFrontmatterChange,
+  };
+}
+
+export type FileContext = ReturnType<typeof useFileContext>;
